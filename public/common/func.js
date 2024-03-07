263224ff9a9c37c7234df60bee153e2a5e91b060
IPSeGue <130346424+IPSeGue@users.noreply.github.com>
2024-03-07 22:22:04 +0800

    wrong data, help delete it

--- public/common/func.js
+++ public/common/func.js
@@ -1,12 +1,1 @@
-const mongoose = require('mongoose');
-
-const reservationSchema = new mongoose.Schema({
-  reservationNumber: String,
-  laboratory: String,
-  time: String,
-  studentId: String
-});
-
-const Reservation = mongoose.model('Reservation', reservationSchema);
-
-module.exports = Reservation;
+help delete 
