e792ab046172586c5cb8026d6d5aa130d8135908
jmcisneros-dlsu <126469947+jmcisneros-dlsu@users.noreply.github.com>
2024-04-01 18:06:26 +0800

    changed

--- models/responder.js
+++ models/responder.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/labDB');
+mongoose.connect('mongodb+srv://johnmaverickcisneros:qFHZMTWstIzxEfkB@cluster0.xld8ueb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://johnmaverickcisneros:qFHZMTWstIzxEfkB@cluster0.xld8ueb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
