96e44e6716a2549bdfbd9ecf8f3046dd29eded89
jmcisneros-dlsu <126469947+jmcisneros-dlsu@users.noreply.github.com>
2024-04-01 20:20:32 +0800

    change

--- models/responder.js
+++ models/responder.js
@@ -1,54 +1,56 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://johnmaverickcisneros:qFHZMTWstIzxEfkB@cluster0.xld8ueb.mongodb.net/');
+const { ServerApiVersion } = require('mongodb');
 
+// Connect to MongoDB Atlas using Mongoose
+mongoose.connect('mongodb+srv://johnmaverickcisneros:qFHZMTWstIzxEfkB@cluster0.xld8ueb.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000 // Timeout after 5 seconds if connection fails
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
+// Define reservation schema
 const reservationSchema = new mongoose.Schema({
-    name: {type: String, default: function() { return this.parent().name; } }, 
-    reservation_id:  {type : String}, 
-    student_id: {type: String, default: function() { return this.parent().student_id; } },
-    laboratory: {type: String}, 
-    room: {type: String}, 
-    date: {type: String},
-    start_time: {type: Number}, 
-    end_time: {type: Number},
-    seat_ids: [{
-        type: String
-    }],
-    equipment: [{
-        type: String
-    }],
-}, {versionKey: false});
+  name: { type: String, default: '' }, 
+  reservation_id:  { type: String }, 
+  student_id: { type: String, default: '' },
+  laboratory: { type: String }, 
+  room: { type: String }, 
+  date: { type: String },
+  start_time: { type: Number }, 
+  end_time: { type: Number },
+  seat_ids: [{ type: String }],
+  equipment: [{ type: String }],
+}, { versionKey: false });
 
-
-// was thinking maybe we split the user and lab tech schema 
+// Define user schema
 const userSchema = new mongoose.Schema({
-    name: {type: String}, 
-    user_id: {type: String},
-    acc_type: {type: String},
-    password: {type: String},
-    image: {type: Buffer},
-    desc : {type: String}
-}, {versionKey: false});
+  name: { type: String }, 
+  user_id: { type: String },
+  acc_type: { type: String },
+  password: { type: String },
+  image: { type: Buffer },
+  desc: { type: String }
+}, { versionKey: false });
 
+// Create models
+const Reservation = mongoose.model('Reservation', reservationSchema);
+const User = mongoose.model('User', userSchema);
 
+// Export models
+module.exports.Reservation = Reservation; 
+module.exports.User = User; 
 
-const reservationModel = mongoose.model('reservation', reservationSchema);
-const userModel = mongoose.model('user', userSchema); 
+// Handle errors
+mongoose.connection.on('error', err => {
+  console.error('MongoDB connection error:', err);
+});
 
-function errorFn(err){
-    console.log('Error found. Please trace!');
-    console.error(err);
-}
-
-function finalClose(){
-    console.log('Close connection at the end!');
-    mongoose.connection.close();
-    process.exit();
-}
-
-
-module.exports.reservationModel = reservationModel; 
-module.exports.userModel = userModel; 
-module.exports.errorFn = errorFn;
-module.exports.finalClose = finalClose; 
-
+// Close connection on process exit
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('MongoDB connection closed');
+    process.exit(0);
+  });
+});
