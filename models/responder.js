f828c2e51f184b78fa9a3a5b4e7226c739446dc3
jmcisneros-dlsu <126469947+jmcisneros-dlsu@users.noreply.github.com>
2024-04-01 20:14:37 +0800

    Add files via upload

--- models/responder.js
+++ models/responder.js
@@ -1,32 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://johnmaverickcisneros:qFHZMTWstIzxEfkB@cluster0.xld8ueb.mongodb.net/');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://johnmaverickcisneros:qFHZMTWstIzxEfkB@cluster0.xld8ueb.mongodb.net/";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
 
 const reservationSchema = new mongoose.Schema({
     name: {type: String, default: function() { return this.parent().name; } }, 
