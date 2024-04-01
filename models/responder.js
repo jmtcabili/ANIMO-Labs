7c708fc09148b6cca2c7482abe5551e020163f0e
jmcisneros-dlsu <126469947+jmcisneros-dlsu@users.noreply.github.com>
2024-04-01 21:59:50 +0800

    Add files via upload

--- models/responder.js
+++ models/responder.js
@@ -1,31 +1,27 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://johnmaverickcisneros:qFHZMTWstIzxEfkB@cluster0.xld8ueb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://johnmaverickcisneros:qFHZMTWstIzxEfkB@cluster0.xld8ueb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// MongoDB connection string with your actual password
+const mongoURI = "mongodb+srv://johnmaverickcisneros:qFHZMTWstIzxEfkB@cluster0.a3jrub1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Connect to MongoDB using mongoose
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB via Mongoose!");
+}).catch(err => {
+  console.error("Error connecting to MongoDB:", err);
+});
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(mongoURI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   }
 });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 async function run() {
   try {
